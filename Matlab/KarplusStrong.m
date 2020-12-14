e44e036bd5bcde0333c9fbb0772d82f6c55ca42e
AdamHoej-au <au617583@post.au.dk>
2020-12-14 21:05:50 +0100

    Twinkle Twinkle Little Star

--- Matlab/KarplusStrong.m
+++ Matlab/KarplusStrong.m
@@ -1,13 +1,14 @@
-function KarplusSound = KarplusStrong(Tone, SampleRate, N, duration)
+function KarplusSound = KarplusStrong(Tone,duration,Klang,SampleRate, N)
     %myFun - Description
     %
     % Syntax:  karplusStrong(Tone,SampleRate,N,Duration)
     %
     % Long description
     DEFAULT_TONE = 262; % C4
+    DEFAULT_KLANG = 0; % C4
     DEFAULT_SAMPLERATE = 20e3;
     DEFAULT_N = 4096;
-    DEFAULT_DURATION = 4;
+    DEFAULT_DURATION = 0.25;
 
     try
 
@@ -17,6 +18,15 @@ function KarplusSound = KarplusStrong(Tone, SampleRate, N, duration)
 
     catch
         Tone = DEFAULT_TONE;
+    end
+    try
+
+        if isempty(Klang)
+            Klang = DEFAULT_KLANG;
+        end
+
+    catch
+        Klang = DEFAULT_KLANG;
     end
 
     try
@@ -77,23 +87,23 @@ function KarplusSound = KarplusStrong(Tone, SampleRate, N, duration)
     % Create coefficients for the filter
     bCoeff = firls(filterOrder, normFreqVector, AmpVector);
 
-    Klang = 0.0;
+
 
     % Include the first sample - exclude samples during the delay and mean the two samples lastly read
-    aCoeff = [1 zeros(1, DelayBin), -0.5 - Klang, -0.5 + Klang + 0.008];
+    aCoeff = [1 zeros(1, DelayBin), -0.5 - Klang, -0.5 + Klang];
 
     % [1 000000000000000000000000000000 -0.5,-0.5]
-    figure(1); clf
-    freqz(bCoeff, aCoeff, f_vek, SampleRate);
-    title('C Tone - Frequency domain');
+    % figure(1); clf
+    % freqz(bCoeff, aCoeff, f_vek, SampleRate);
+    % title('C Tone - Frequency domain');
 
-    [H, W] = freqz(bCoeff, aCoeff, f_vek, SampleRate);
+    % [H, W] = freqz(bCoeff, aCoeff, f_vek, SampleRate);
 
-    figure(2); clf
-    plot(W, 20 * log10(abs(H)));
-    title('C Tone - Frequency domain');
-    xlabel('Frequency (Hz)');
-    ylabel('Magnitude (dB)');
+    % figure(2); clf
+    % % plot(W, 20 * log10(abs(H)));
+    % % title('C Tone - Frequency domain');
+    % % xlabel('Frequency (Hz)');
+    % ylabel('Magnitude (dB)');
     grid on
 
     %%% Filter creation
@@ -105,12 +115,12 @@ function KarplusSound = KarplusStrong(Tone, SampleRate, N, duration)
     note = note - mean(note);
     note = note / max(abs(note)); % Normalization
 
-    time = 1:(length(note));
-    length(time)
-    figure(3);
-    plot(time * t_res, note)
-    xlabel('Time (s)')
-    KarplusSound = audioplayer(note, SampleRate); 
+    % time = 1:(length(note));
+    % length(time)
+    % figure(3);
+    % plot(time * t_res, note)
+    % xlabel('Time (s)')
+    KarplusSound = note; 
 end
     %% Tanke
 
--- Matlab/KarplusTester.m
+++ Matlab/KarplusTester.m
@@ -1,2 +1,23 @@
-KarplusSound = KarplusStrong();
-play(KarplusSound);
+Fs = 20e3;
+Duration = 0.5;
+PauseDuration = 0.5*Duration;
+LongPauseDuration = Duration;
+C = KarplusStrong(261.626, Duration);
+D = KarplusStrong(293.665,Duration);
+E = KarplusStrong(329.628,Duration);
+F = KarplusStrong(349.228,Duration,0.2);
+G = KarplusStrong(391.995,Duration,0.1);
+A = KarplusStrong(440,Duration);
+B = KarplusStrong(493.883,Duration);
+Ch = KarplusStrong(2*261.626,4*Duration,0.4);
+
+Default = KarplusStrong();
+
+% playArray = [C; D; E; F; G; A;B;Ch];
+% SoundPlayer = audioplayer(playArray, Fs)
+% play(SoundPlayer)
+
+
+Twinkle = [C; C; G; G; A; A; G; zeros(PauseDuration * Fs, 1); F; F; E; E; D; D; C; zeros(PauseDuration * Fs, 1); G; G; F; F; E; E; D; zeros(PauseDuration * Fs, 1); G; G; F; F; E; E; D; zeros(LongPauseDuration * Fs, 1); C; C; G; G; A; A; G; zeros(PauseDuration * Fs, 1); F; F; E; E; D; D; Ch];
+TwinklePlayer = audioplayer(Twinkle, Fs);
+play(TwinklePlayer)
