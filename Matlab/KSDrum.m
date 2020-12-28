bf553f17378c8b1ba9267a59b9dbb61660f8723f
Rasmus Kahr <dev@kahr.me>
2020-12-28 15:17:07 +0100

    Set drum initial condition to all ones

--- Matlab/KSDrum.m
+++ Matlab/KSDrum.m
@@ -1,52 +1,51 @@
 function KSDrumSound = KSDrum(PValue,b,Duration,Fs)
+  
+  DEFAULT_PValue = 200; % C note
+  DEFAULT_B = 0.5;
+  DEFAULT_DURATION = 0.4;
+  DEFAULT_SAMPLERATE = 20e3;
 
-    DEFAULT_PValue = 200; % C note
-    DEFAULT_B = 0.5;
-    DEFAULT_DURATION = 0.4;
-    DEFAULT_SAMPLERATE = 20e3;
-    if nargin < 1
-        PValue = DEFAULT_PValue;
-    end
-
-    if nargin < 2
-        b = DEFAULT_B;
-    end
-
-    if nargin < 3
-        Duration = DEFAULT_DURATION;
-    end
-    if nargin < 4
-        Fs = DEFAULT_SAMPLERATE;
-    end
-
+  if nargin < 1
+    PValue = DEFAULT_PValue;
+  end
+  
+  if nargin < 2
+    b = DEFAULT_B;
+  end
+  
+  if nargin < 3
+    Duration = DEFAULT_DURATION;
+  end
+  if nargin < 4
+    Fs = DEFAULT_SAMPLERATE;
+  end
+  
+  % Adding the initial conditions
+  % y = [randi([0, 1], [1, PValue]) zeros(1, Fs * Duration)];
+  y = [ones(1,Fs*Duration)];
+  OutputLength = numel(y);
+  
+  randomValue = rand(1, OutputLength);
+  
+  PrevValue = 0; % Avoid the negative index at y(1)
+  
+  for idx = PValue + 1:PValue + OutputLength
     
-    % Adding the initial conditions
-    y = [randi([0, 1], [1, PValue]) zeros(1, Fs * Duration)];
-    % y = [ones(1,Fs*Duration)];
-    OutputLength = numel(y);
-
-
-    randomValue = rand(1, OutputLength);
-
-    PrevValue = 0; % Avoid the negative index at y(1)
-
-for idx = PValue + 1:PValue + OutputLength
-
     if randomValue(idx - PValue) <= b
-        y(idx) = (y(idx - PValue) + PrevValue) / 2;
+      y(idx) = (y(idx - PValue) + PrevValue) / 2;
     else
-        y(idx) = -(y(idx - PValue) + PrevValue) / 2;
+      y(idx) = -(y(idx - PValue) + PrevValue) / 2;
     end
-
+    
     PrevValue = y(idx - PValue);
-end
-
-note = y;
-%% Subtract the DC offset
-note = note - mean(note);
-%% Normalize the sound ; Range [0:1]
-note = note / max(abs(note)); % Normalization
-
-KSDrumSound = note;
-
+  end
+  
+  note = y;
+  %% Subtract the DC offset
+  note = note - mean(note);
+  %% Normalize the sound ; Range [0:1]
+  note = note / max(abs(note)); % Normalization
+  
+  KSDrumSound = note;
+  
 end
